82d668b7065e769210ee3321c3c2a0a574434b40
Jame9703 <ncsb1213444@outlook.com>
2025-06-01 22:03:14 +0800

    #

--- MainWindow.xaml.cpp
+++ MainWindow.xaml.cpp
@@ -8,6 +8,8 @@
 
 using namespace winrt;
 using namespace Microsoft::UI::Xaml;
+using namespace Microsoft::UI::Xaml::Controls;
+using namespace Microsoft::UI::Xaml::Controls::Primitives;
 using namespace Windows::Foundation;
 
 // To learn more about WinUI, the WinUI project structure,
@@ -19,6 +21,8 @@ namespace winrt::RSSN::implementation
 	{
 		InitializeComponent();
 		SetTitleBar(AppTitleBar());
+		HomePageButton().IsChecked(true);
+		_lastSelectedButton = HomePageButton();//确保开始时_lastSelectedButton不为null
 	}
 	int32_t MainWindow::MyProperty()
 	{
@@ -29,13 +33,153 @@ namespace winrt::RSSN::implementation
 	{
 		throw hresult_not_implemented();
 	}
-
-	void MainWindow::ClassNameHyperlinkButton_Click(IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args)
+	void MainWindow::StartAnimation(ToggleButton const& button)
 	{
+		if (button != nullptr)
+		{
+			if (_lastSelectedButton != nullptr)
+			{
+				// 如果之前有选中的按钮，取消其选中状态
+				_lastSelectedButton.IsChecked(false);
+				auto oldbutton_children = (_lastSelectedButton.Content().as<StackPanel>()).Children();
+				for (auto&& child : oldbutton_children)
+				{
+					if (auto textBlock = child.try_as<TextBlock>())
+					{
+						textBlock.Visibility(Visibility::Visible);//显示_lastSelectedButton的文本
+					}
+				}
+				auto newbutton_children = (button.Content().as<StackPanel>()).Children();
+				for (auto&& child : newbutton_children)
+				{
+					if (auto textBlock = child.try_as<TextBlock>())
+					{
+						textBlock.Visibility(Visibility::Collapsed);//隐藏本次点击按钮的文本
+					}
+				}
+			}
 
+			button.IsChecked(true); // 设置当前按钮为选中状态
+			auto targetPosition = button.TransformToVisual(Canvas()).TransformPoint(Point(button.ActualWidth() / 2, (button.ActualHeight()) / 2));
+			// 设置Storyboard的动画值
+			if (doubleAnimation() != nullptr)
+			{
+				doubleAnimation().To() = targetPosition.Y - MovingRectangle().Height();
+			}
+			MovingRectangle().Visibility(Visibility::Visible);
+			rectangle().Visibility(Visibility::Collapsed);
+			// 开始Storyboard动画
+			MoveRectangleStoryboard().Begin();
+
+			_lastSelectedButton = button; // 更新最后选中的按钮
+			HomeIcon().Glyph(L"\uE80F");
+			ClassIcon().Glyph(L"\uE77B");
+			NumbersIcon().Visibility(Visibility::Visible);
+			NumbersIconChecked().Visibility(Visibility::Collapsed);
+			SettingsIcon().Glyph(L"\uE713");
+		}
 	}
-	void ClassNameHyperlinkButton_Click(IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args)
-	{
 
+	void MainWindow::HomePageButton_Click(IInspectable const& sender, RoutedEventArgs const& e)
+	{
+		auto button = sender.as<ToggleButton>();
+		if (_lastSelectedButton != button)
+		{
+			StartAnimation(button);
+			//ContentFrame().Navigate(xaml_typename<HomePage>());
+		}
+		else
+		{
+			button.IsChecked(true);//上次点击的按钮和本次一样，保持选中状态
+		}
+		HomeIcon().Glyph(L"\uEA8A");
+	}
+
+	void MainWindow::ClassPageButton_Click(IInspectable const& sender, RoutedEventArgs const& e)
+	{
+		auto button = sender.as<ToggleButton>();
+		if (_lastSelectedButton != button)
+		{
+			StartAnimation(button);
+			//ContentFrame().Navigate(xaml_typename<ClassPage>());
+		}
+		else
+		{
+			button.IsChecked(true);//上次点击的按钮和本次一样，保持选中状态
+		}
+		ClassIcon().Glyph(L"\uEA8C");
+	}
+
+	void MainWindow::NumbersPageButton_Click(IInspectable const& sender, RoutedEventArgs const& e)
+	{
+		auto button = sender.as<ToggleButton>();
+		if (_lastSelectedButton != button)
+		{
+			StartAnimation(button);
+			//ContentFrame().Navigate(xaml_typename<NumbersPage>());
+		}
+		else
+		{
+			button.IsChecked(true);//上次点击的按钮和本次一样，保持选中状态
+		}
+		NumbersIcon().Visibility(Visibility::Collapsed);
+		NumbersIconChecked().Visibility(Visibility::Visible);
+	}
+
+	void MainWindow::CharactersPageButton_Click(IInspectable const& sender, RoutedEventArgs const& e)
+	{
+		auto button = sender.as<ToggleButton>();
+		if (_lastSelectedButton != button)
+		{
+			StartAnimation(button);
+			//ContentFrame().Navigate(xaml_typename<CharactersPage>());
+		}
+		else
+		{
+			button.IsChecked(true);//上次点击的按钮和本次一样，保持选中状态
+		}
+	}
+
+	void MainWindow::HelpPageButton_Click(IInspectable const& sender, RoutedEventArgs const& e)
+	{
+		auto button = sender.as<ToggleButton>();
+		if (_lastSelectedButton != button)
+		{
+			StartAnimation(button);
+			//ContentFrame().Navigate(xaml_typename<HelpPage>());
+		}
+		else
+		{
+			button.IsChecked(true);//上次点击的按钮和本次一样，保持选中状态
+		}
+	}
+
+	void MainWindow::MorePageButton_Click(IInspectable const& sender, RoutedEventArgs const& e)
+	{
+		auto button = sender.as<ToggleButton>();
+		if (_lastSelectedButton != button)
+		{
+			StartAnimation(button);
+			//ContentFrame().Navigate(xaml_typename<MorePage>());
+		}
+		else
+		{
+			button.IsChecked(true);//上次点击的按钮和本次一样，保持选中状态
+		}
+	}
+
+	void MainWindow::SettingsPageButton_Click(IInspectable const& sender, RoutedEventArgs const& e)
+	{
+		auto button = sender.as<ToggleButton>();
+		if (_lastSelectedButton != button)
+		{
+			StartAnimation(button);
+			//ContentFrame().Navigate(xaml_typename<SettingsPage>());
+		}
+		else
+		{
+			button.IsChecked(true);//上次点击的按钮和本次一样，保持选中状态
+		}
+		SettingsIcon().Glyph(L"\uF8B0");
 	}
 }
--- MainWindow.xaml.h
+++ MainWindow.xaml.h
@@ -1,27 +1,36 @@
 #pragma once
 
 #include "MainWindow.g.h"
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+using namespace Microsoft::UI::Xaml::Controls;
+using namespace Microsoft::UI::Xaml::Controls::Primitives;
+using namespace Windows::Foundation;
 
 namespace winrt::RSSN::implementation
 {
-    struct MainWindow : MainWindowT<MainWindow>
-    {
-        MainWindow();
-        //{
-        //    // Xaml objects should not call InitializeComponent during construction.
-        //    // See https://github.com/microsoft/cppwinrt/tree/master/nuget#initializecomponent
-        //    this->ExtendsContentIntoTitleBar(true);
-        //    this->SetTitleBar(AppTitleBar());
-        //}
-        int32_t MyProperty();
-        void MyProperty(int32_t value);
-        void ClassNameHyperlinkButton_Click(IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
-    };
+	struct MainWindow : MainWindowT<MainWindow>
+	{
+		MainWindow();
+		// Xaml objects should not call InitializeComponent during construction.
+		// See https://github.com/microsoft/cppwinrt/tree/master/nuget#initializecomponent
+		int32_t MyProperty();
+		void MyProperty(int32_t value);
+		ToggleButton  _lastSelectedButton;
+		void HomePageButton_Click(IInspectable const& sender, RoutedEventArgs const& e);
+		void ClassPageButton_Click(IInspectable const& sender, RoutedEventArgs const& e);
+		void NumbersPageButton_Click(IInspectable const& sender, RoutedEventArgs const& e);
+		void CharactersPageButton_Click(IInspectable const& sender, RoutedEventArgs const& e);
+		void HelpPageButton_Click(IInspectable const& sender, RoutedEventArgs const& e);
+		void MorePageButton_Click(IInspectable const& sender, RoutedEventArgs const& e);
+		void SettingsPageButton_Click(IInspectable const& sender, RoutedEventArgs const& e);
+		void StartAnimation(ToggleButton const& button);
+	};
 }
 
 namespace winrt::RSSN::factory_implementation
 {
-    struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
-    {
-    };
+	struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
+	{
+	};
 }
